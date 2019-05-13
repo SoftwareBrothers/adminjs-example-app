bc199674de53a3fda85998a753586421cf6f0609
wojtek-krysiak <wojciech.g.krysiak@gmail.com>
2019-05-13 13:17:28 +0200

    bump admin-bro version

--- admin/resources/user.js
+++ admin/resources/user.js
@@ -21,16 +21,9 @@ module.exports = {
       guard: 'You can setup guards before an action - just in case.',
       component: AdminBro.require('../components/dont-touch-this-action'),
       handler: async (request, response, data) => {
-        const page = new PageBuilder({ admin: data._admin })
-        page.addTextBox({
-          columns: 6,
-          content: '<img src="https://cathumor.net/wp-content/uploads/2015/04/cat-humor-dangerous-cat-is-dangerous.jpg">'
-        })
-        page.addTextBox({
-          columns: 3,
-          content: 'Didn\'t expected that?'
-        })
-        return page.toHTML()
+        return {
+          record: data.record.toJSON()
+        }
       }
     }
   }
