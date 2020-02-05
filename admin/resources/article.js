bfc5376b589f0c6ae01508bf6a380d365f7398b9
wojtek-krysiak <wojciech.g.krysiak@gmail.com>
2020-02-05 22:18:01 +0100

    chore: bump admin vesion

--- admin/resources/article.js
+++ admin/resources/article.js
@@ -4,6 +4,11 @@ const { sort, timestamps } = require('./sort')
 module.exports = {
   name: 'Article (customize field)',
   sort,
+  actions: {
+    edit: {
+      showInDrawer: false,
+    }
+  },
   properties: {
     ...timestamps,
     _id: { isVisible: false },
