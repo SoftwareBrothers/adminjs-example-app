e6fc9e5322f9eccd1b9a0026571bc87ee6b06ecf
Arian Sobczak <arian.sobczak@rst.com.pl>
2022-03-16 14:33:39 +0100

    chore: fixed linting

--- src/admin/pages/design-system-example.tsx
+++ src/admin/pages/design-system-example.tsx
@@ -1,10 +1,10 @@
-import { Box, H4, Label } from '@adminjs/design-system';
+import { Box, H4 } from '@adminjs/design-system';
 import React, { FC } from 'react';
 import { ModalExample } from '../components/design-system-examples';
 
 const DesignSystemExample: FC = () => {
   return (
-    <Box m="xl" variant='white'>
+    <Box m="xl" variant="white">
       <Box>
         <H4>Modal example</H4>
         <ModalExample />
