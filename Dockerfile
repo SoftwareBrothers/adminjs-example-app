8d4703b6a1c0be466874f667dec599d1e4d540f7
Rafal Dziegielewski <rafal.dzieg@gmail.com>
2022-04-26 13:05:28 +0200

    chore: add workflow for demo release

--- Dockerfile
+++ Dockerfile
@@ -8,7 +8,7 @@ ENV TZ="UTC"
 COPY package.json ./
 COPY yarn.lock ./
 
-RUN yarn install --frozen-lockfile --production
+RUN yarn install
 COPY . .
 
 RUN npm i -g typescript
@@ -16,7 +16,7 @@ RUN yarn build
 RUN npx prisma generate
 RUN rm -rf src
 
-ENV ADMIN_JS_SKIP_BUNDLE="true"
+#ENV ADMIN_JS_SKIP_BUNDLE="true"
 
 EXPOSE 3000
 CMD yarn start
--- src/servers/express.ts
+++ src/servers/express.ts
@@ -1,17 +1,21 @@
-import { createAdmin, generateAdminJSConfig } from '../admin';
 import mongoose from 'mongoose';
+import AdminJS from 'adminjs';
+import cors from 'cors';
 import { expressAuthenticatedRouter } from '../admin/router';
 import { init } from '../sources/mikroorm/config';
 import { connection } from '../sources/typeorm/config';
-import AdminJS from 'adminjs';
+import { createAdmin, generateAdminJSConfig } from '../admin';
 
 const express = require('express');
 const app = express();
+app.use(cors());
 
 const attachAdminJS = async () => {
   const config = generateAdminJSConfig();
   const adminJS = new AdminJS(config);
-  app.use(adminJS.options.rootPath, expressAuthenticatedRouter(adminJS));
+  const adminRouter = expressAuthenticatedRouter(adminJS);
+  app.use(adminJS.options.rootPath, adminRouter);
+
   await createAdmin();
 };
 
