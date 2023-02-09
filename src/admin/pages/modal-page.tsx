b1f23081f51de1c3e096b1434328caec03a5ce8e
Arian Sobczak <arian.sobczak@rst.com.pl>
2023-02-09 17:10:01 +0100

    chore: design system lift

---
+++ src/admin/pages/modal-page.tsx
@@ -0,0 +1,16 @@
+import { Box, Header } from '@adminjs/design-system';
+import React, { FC } from 'react';
+import { ModalExample } from '../components/design-system-examples';
+
+const ModalPage: FC = () => {
+  return (
+    <Box variant="grey">
+      <Header>Modal </Header>
+      <Box variant="white">
+        <ModalExample />
+      </Box>
+    </Box>
+  );
+};
+
+export default ModalPage;
