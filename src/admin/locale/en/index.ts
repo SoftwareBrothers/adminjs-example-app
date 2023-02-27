35524f25670e5231cac70f9cf2331d145110e3c3
Jarosław Wasiak <jaroslaw.wasiak@rst.com.pl>
2023-02-27 14:00:24 +0000

    chore: component translations example

--- src/admin/locale/en/index.ts
+++ src/admin/locale/en/index.ts
@@ -1,5 +1,7 @@
 import type { Locale } from 'adminjs';
 import * as Common from './common.json';
+import * as Components from './components.json';
+
 import * as Complicated from './complicated.json';
 import * as Person from './person.json';
 
@@ -7,6 +9,7 @@ const enLocale: Omit<Locale, 'availableLanguages'> = {
   language: 'en',
   translations: {
     ...Common,
+    ...Components,
     resources: {
       Complicated,
       Person,
