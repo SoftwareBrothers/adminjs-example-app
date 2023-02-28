f33b059182cbbdf8f0452d4dbfa25f88a7361838
Arian Sobczak <arian.sobczak@rst.com.pl>
2023-02-28 12:52:56 +0100

    chore: store multiple languages with default locale conf

--- src/admin/locale/index.ts
+++ src/admin/locale/index.ts
@@ -1,7 +1,19 @@
-// import en from './en';
+import { locales as AdminJSLocales } from 'adminjs';
 import de from './de';
+import en from './en';
+
+const availableLanguages = {
+  ...AdminJSLocales,
+  en,
+  de,
+};
+
+const localeKey = process.env.LOCALE || 'en';
+const locale = availableLanguages[localeKey];
+
+console.log(`Selected locale - "%s"`, localeKey);
 
 export default {
-  ...de,
-  availableLanguages: ['en', 'de', 'it', 'pl', 'pt-br', 'ua', 'zh-cn'],
+  ...locale,
+  availableLanguages: Object.keys(availableLanguages),
 };
