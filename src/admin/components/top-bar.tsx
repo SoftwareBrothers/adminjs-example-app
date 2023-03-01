ed6de9b56fcd036e80e63968b79b4475ec1f27a2
Arian Sobczak <arian.sobczak@rst.com.pl>
2023-03-01 12:29:03 +0100

    chore: use component loader in example app

--- src/admin/components/top-bar.tsx
+++ src/admin/components/top-bar.tsx
@@ -1,8 +1,22 @@
+import { Box, Button, Icon } from '@adminjs/design-system';
 import React from 'react';
 
-const TopBar = () => {
-  return <div>Custom top bar</div>;
-};
+const TopBar = () => (
+  <Box flex flexGrow={1} justifyContent="end" alignItems="center">
+    <Button color="text" as="a" href="https://adminjs.page.link/slack" target="_blank">
+      <Icon icon="Slack" />
+      Slack
+    </Button>
+    <Button color="text" as="a" href="https://github.com/SoftwareBrothers/adminjs/issues" target="_blank">
+      <Icon icon="GitHub" />
+      GitHub
+    </Button>
+    <Button color="text" as="a" href="https://docs.adminjs.co" target="_blank">
+      <Icon icon="BookOpen" />
+      Documentation
+    </Button>
+  </Box>
+);
 
 export { TopBar };
 export default TopBar;
